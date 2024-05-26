24fbe1b46b803c1c1af8dfa1576d2c60a3abae2c
Linn Thit <lth1212001@gmail.com>
2024-05-26 17:28:23 +0630

    -

--- DotNet8.EFCoreRawSqlSample/Program.cs
+++ DotNet8.EFCoreRawSqlSample/Program.cs
@@ -1,13 +1,10 @@
-
 using DotNet8.EFCoreRawSqlSample.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,7 +15,6 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
